557515e19f88dd8c5f9b28c8f24880a7c63c9514
antalexa <antalexa@Antalexa-MacBook-Pro.local>
2024-08-07 12:15:04 +0530

    initial commit for rafiki kitchen online ordering website

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-4xl font-bold">Rafiki Kitchen</h1>
+      <p className="text-lg">
+        The best artisinal sandwiches in Bangalore
+      </p>
+    </main>
+  );
+}
