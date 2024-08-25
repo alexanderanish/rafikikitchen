144c00e40d986246fd0061d7c6f0ed456d50efd8
antalexa <antalexa@Antalexa-MacBook-Pro.local>
2024-08-25 16:51:22 +0530

    fixed some currency and alignnment

--- components/OrderSummary.tsx
+++ components/OrderSummary.tsx
@@ -13,7 +13,7 @@ export default function OrderSummary() {
       {cart.map((item) => (
         <div key={item.id} className="flex justify-between mb-2">
           <span>{item.name} x {item.quantity}</span>
-          <span>${(item.price * item.quantity).toFixed(2)}</span>
+          <span>₹{(item.price * item.quantity).toFixed(2)}</span>
         </div>
       ))}
       <div className="border-t mt-4 pt-4">
